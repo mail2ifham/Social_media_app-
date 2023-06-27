842daa5ad468750a44d95c84f5be585348c88fb5
ifham <mail2ifham@gmail.com>
2023-06-27 13:14:27 +0530

    2nd commint

---
+++ src/PostLayout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link, Outlet} from "react-router-dom"
+
+const PostLayout = () => {
+  return (
+    <>
+      <Link to="/postpage/1">Post1</Link> <br />
+      <Link to="/postpage/2">Post2</Link> <br />
+      <Link to="/postpage/3">Post3</Link>
+      <Outlet />
+    </>
+  );
+};
+
+export default PostLayout;
