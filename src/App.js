7bbd34756904a9c840780955d85ea7c20b2d9546
ifham <mail2ifham@gmail.com>
2023-06-27 13:08:09 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import Post from "./Post";
+import Missing from "./Missing";
+import PostLayout from "./PostLayout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/newpost">NewPost</Link>
+          </li>
+          <li>
+            <Link to="/postpage">PostPage</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/newpost" element={<NewPost />} />
+        <Route path="/postpage" element={<PostLayout />}>
+          <Route index element={<PostPage />} />
+          <Route path=":id" element={<Post />} />
+          <Route path="*" element={<Missing />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
